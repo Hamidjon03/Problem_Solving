a513d8310bfc56d4f5ad75a8c2cbf8d9250f181a
Hamidjon03 <forevalm@gmail.com>
2023-08-31 10:47:25 +0500

    5-day_5-problem

--- 4-day.js
+++ 4-day.js
@@ -25,7 +25,16 @@ var createCounter = function (init) {
   }
 };
 
-
+/**
+ * var createCounter = function(init) {
+    let presentCount = init
+    return {
+        increment:()=> ++presentCount,
+        decrement:()=> --presentCount,
+        reset:()=> presentCount = init,
+    }
+};
+ */
 const counter = createCounter(5)
 counter.increment(); // 6
 counter.reset(); // 5
---
+++ 5-day.js
@@ -0,0 +1,9 @@
+// 2635. Apply Transform Over Each Element in Array
+
+var map = function (arr, fn) {
+  for (let i = 0; i < arr.length; ++i) {
+    arr[i] = fn(arr[i], i);
+  }
+  return arr;
+
+};
